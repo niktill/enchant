69b45f961542cc38205fadb0239367985104b4f0
Nik Till <niktill123@gmail.com>
2020-09-10 11:37:41 -0400

    enchant on firebase

---
+++ client/build/precache-manifest.85f63b5662ba7c22b8815dbc36054268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4cc1f166c476d798831dd2339f234f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4b2af14ab65fd55c5a0",
+    "url": "/static/js/2.ed5b7694.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/static/js/2.ed5b7694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ed7f86bfe7e91fad7d",
+    "url": "/static/js/main.d642197d.chunk.js"
+  },
+  {
+    "revision": "bbe0a3d62e3fe3528fb8",
+    "url": "/static/js/runtime-main.466d4c15.js"
+  }
+]);
